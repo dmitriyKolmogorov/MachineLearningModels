dbd74f9a910e05eeb255055364dfc519b40fac89
myDiamondsDancing <66516985+dmitriyKolmogorov@users.noreply.github.com>
2020-06-09 14:05:33 +0300

    Create setup.py
    This file setups Python package named my_machine_learning.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="my_machine_learning",
+      version="0.0.1",
+      author="Kolmogorov Dmitriy",
+      author_email="kolmogorov.dmitro@gmail.com",
+      description="A small project with machine learning models",
+      url="https://github.com/dmitriyKolmogorov/MachineLearningModels",
+      packages=find_packages())
